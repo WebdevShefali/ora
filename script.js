bb296d4774e92d247d8c0bb4325d6c07cb8a0511
WebdevShefali <jangidshefali62@gmail.com>
2023-03-10 17:28:30 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function changeBtn(x) {
     x.classList.toggle("change");
   }
+// scroll to top
+  let scrollBtn =   document.querySelector(".topBtn")  ;
+  window.onscroll = function () {
+scrollFunction();
+};
+
+function scrollFunction() {
+if (
+document.body.scrollTop > 20 ||
+document.documentElement.scrollTop > 20
+) {
+scrollBtn.style.display = "block";
+} else {
+scrollBtn.style.display = "none";
+}
+}
+scrollBtn.addEventListener("click", backToTop);
+
+function backToTop() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
